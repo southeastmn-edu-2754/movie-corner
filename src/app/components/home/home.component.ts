49483a56ec777b7a6858aefbfc66255fb5398ecd
mswanson-mscst <mswanson@southeastmn.edu>
2020-02-18 15:05:35 -0600

    Initial commit, Angular project

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
-  title: string = "COMC2749 Angular Exercises";
+  title: string = "COMC2754 Movie Corner";
 
   constructor() { }
 
